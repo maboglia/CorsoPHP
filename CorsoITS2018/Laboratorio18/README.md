278af8a96e42ec89e0fb617fea856131fb9e5e93
maboglia <mauro.bogliaccino@gmail.com>
2018-10-09 12:09:40 +0200

    upd

---
+++ CorsoITS2018/Laboratorio18/README.md
@@ -0,0 +1,24 @@
+# Programma
+
+## Progetto php-simple-mvc
+
+[scarica il progetto](https://github.com/maboglia/simple_mvc_php)
+
+esercizi
+
+* usa git clone
+* rifai il progetto step by step
+
+## Teoria: collegamento database con PDO
+
+[scarica il progetto](https://github.com/maboglia/StudentePDO)
+
+appunti
+
+* [database](https://github.com/maboglia/CorsoPHP/blob/master/appunti/07_0_database.md)
+* [database PDO](https://github.com/maboglia/CorsoPHP/blob/master/appunti/07_1_PDO.md)
+
+## PHP e OOP - Esempio auto-concessionario
+
+[concessionario auto](./proj01_auto)
+
---
+++ CorsoITS2018/Laboratorio18/proj01_auto/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
+
+    <title>Static Top Navbar Example for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+
+  </head>
+
+  <body>
+
+    <!-- Static navbar -->
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Mauro Bogliaccino</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Corso ITS <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="aulaITS2017.php?list=true">Elenco Studenti</a></li>
+                <li><a href="aulaITS2017.php?grid=true">Griglia Studenti</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://getbootstrap.com/examples/navbar/">Default</a></li>
+            <li class="active"><a href="http://getbootstrap.com/examples/navbar-static-top/">Static top <span class="sr-only">(current)</span></a></li>
+            <li><a href="http://getbootstrap.com/examples/navbar-fixed-top/">Fixed top</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
